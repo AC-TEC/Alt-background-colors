f1825e6866faa4ed31851d9f4d4c0ea9bf726b96
Andy Cocha <andycocha@Mac.lan>
2025-04-22 14:09:56 -0400

    FINISHED

--- alt-background-website/src/App.jsx
+++ alt-background-website/src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from 'react' //used to keep track of the current index
 import './App.css'
 
+
+const background_colors = ['#e63946', '#ffffff', '#a8dadc', '#457b9d', '#1d3557'];
+
 function App() {
   const [count, setCount] = useState(0)
 
+  const buttonClick = () => {
+    setCount((index) => (index + 1) % background_colors.length); //wraps back to first color
+  };
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="alt-background" style={ {backgroundColor: background_colors[count]} } >
+    
+      <button className="created-button" onClick={buttonClick}>
+        Change background
+      </button>
+
+    </div>
+  );
 }
 
 export default App
